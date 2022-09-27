a62481c7751e1b5a1f63f4f787b979c19a405707
uniquelise01 <han19008@byui.edu>
2022-09-27 11:04:04 -0600

    finishing contacts components

--- src/app/contacts/contact-item/contact-item.component.ts
+++ src/app/contacts/contact-item/contact-item.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
-
 import { Contact } from '../contact.model';
 
 @Component({
-  selector: 'cms-contact-detail',
-  templateUrl: './contact-detail.component.html',
-  styleUrls: ['./contact-detail.component.css']
+  selector: 'cms-contact-item',
+  templateUrl: './contact-item.component.html',
+  styleUrls: ['./contact-item.component.css']
 })
-export class ContactDetailComponent implements OnInit {
+export class ContactItemComponent implements OnInit {
   @Input() contact: Contact;
-  
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../contact.model';
 
 
@@ -8,6 +8,9 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
+  
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+
   contacts: Contact[] = [
   new Contact("1", "R. Kent Jackson", "jacksonk@byui.edu", "208-496-3771", "../../assets/images/jacksonk.jpg", null),
   new Contact("2", "Rex Barzee", "barzeer@byui.edu", "208-496-3768", "../../assets/images/barzeer.jpg", null)
@@ -16,6 +19,10 @@ export class ContactListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
   }
 
 }
